c1a2004939e17d92bc3fa7ceb1dc0c6642377912
zakew <90333432+zakew@users.noreply.github.com>
2022-03-02 03:07:20 +0100

    Lagt till function specifications, och main'

--- haskmanager.hs
+++ haskmanager.hs
@@ -1,6 +1,10 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 import System.IO ()
 import Test.HUnit
+import Data.Char
+
+-- Project group 30: Agron Metaj, Pouria Karami, Zakarie Warsame
+-- The HaskMonitor
 
 
 {-  A binary-tree with different task-categorys which are polymorphic labels
@@ -19,14 +23,15 @@ type Task = (String, Bool)
 
 -- Modified data-tree operations. Courtesy of Johannes Borgström and PKD-team
 
-{- exists t v
- ...
-   RETURNS: True iff v is in t
--}
-findAll :: TaskTree a -> Tasklist
-findAll Void  = []
-findAll (Node l _ list r) = findAll l ++ list ++ findAll r
-
+{-  exists t a
+    DESCRIPTION: A function that checks if a category exists in a tree
+    RETURNS: A boolean value representing if the category exists, true or false.
+    EXAMPLES: exists (Node Void "Home" [("Clean",False),("Cook",True)] (Node Void "Work" [("Deadline",False)] Void)) "Work" ==
+              True
+              exists (Node Void "Home" [("Clean",False),("Cook",True)] (Node Void "Work" [("Deadline",False)] Void)) "School" ==
+              False
+    VARIANT: The amount of nodes in the tree respectively their tasklists.
+-} 
 exists :: (Ord a) => TaskTree a -> a -> Bool
 exists Void _ = False
 exists (Node l y list r) x 
@@ -34,43 +39,72 @@ exists (Node l y list r) x
                     | y < x  = exists r x
                     | y > x  = exists l x
 
+{-  delete t a
+    DESCRIPTION: A function that deletes the node equivalent to the input 
+    RETURNS: A new tree without the node
+    EXAMPLES: delete (Node Void "Home" [("Clean",False),("Cook",True)] (Node Void "Work" [("Deadline",False)] Void)) "Work"   ==
+              Node Void "Home" [("Clean",False),("Cook",True)] Void
+              delete (Node Void "Home" [("Clean",False),("Cook",True)] Void)  "Home" ==
+              Void
+    VARIANT: The amount of nodes in the tree.
+-} 
+
 delete :: Eq a => TaskTree a -> a -> TaskTree a 
 delete Void _ = Void
 delete t@(Node l x list r) y
   | x == y    = deleteRoot t
-  | otherwise = (Node (delete l y) x list (delete r y))
+  | otherwise = Node (delete l y) x list (delete r y)
   where
     deleteRoot :: TaskTree a -> TaskTree a
     deleteRoot (Node Void _ _ Void)               = Void
     deleteRoot (Node t x list Void)               = deleteRoot (Node Void x list t)
     deleteRoot (Node l _ _ r@(Node rl x list rr)) = Node l x list $ deleteRoot r
 
-{- insert t v
- ...
-   RETURNS: A new tree with v and all values in t
--}
+{-  insert t a
+    DESCRIPTION: A function that inserts the node equivalent to the first input and a accompanying list of tasks 
+    RETURNS: A new tree with the updated node and its list
+    EXAMPLES: insert Void "Home" [("Clean", False), ("Cook", True)] ==
+              Node Void "Home" [("Clean",False),("Cook",True)] Void
+    VARIANT: The amount of nodes in the tree.
+-} 
+
 insert :: (Ord a) => TaskTree a -> a -> Tasklist -> TaskTree a
-insert Void x list = (Node Void x list Void)
+insert Void x list = Node Void x list Void
 insert (Node l y list r) x list'
-                      | y == x = (Node l y list' r)
-                      | y < x  = (Node l y list (insert r x list'))
-                      | y > x  = (Node (insert l x list') y list r)
+                      | y == x = Node l y list' r
+                      | y < x  = Node l y list (insert r x list')
+                      | y > x  = Node (insert l x list') y list r
 
--- Modified data-tree operations. Courtesy of Johannes Borgström and PKD-team
+-- End of modified data-tree operations. Courtesy of Johannes Borgström and PKD-team
+
+{-  findAll t
+    DESCRIPTION: A function that returns a list containing all the tasks from the tasktree.
+    RETURNS: A Tasklist.
+    EXAMPLES: findAll (Node Void "Home" [("Clean",False),("Cook",True)] (Node Void "Work" [("Deadline",False)] Void)) ==
+              [("Clean",False),("Cook",True),("Deadline",False)]
+    VARIANT: The amount of nodes in the tree respectively their tasklists.
+-}
+findAll :: TaskTree a -> Tasklist
+findAll Void  = []
+findAll (Node l _ list r) = findAll l ++ list ++ findAll r
 
 
+{- main
+   DESCRIPTION: The function that initiates the program and prints the menu and offers the user options
+   EXAMPLES: Får väl testa när det funkar och se
+   SIDE-EFFECTS: Bara Gud vet hur många som finns.
+-}
 main :: IO ()
 main = do
     contents <- readFile "Test.txt" -- Läser in lagrad data från en textfil               ? hur kan man nå lagrad data ?
     
-    putStrLn  "\nWelcome to your HaskMonitor\n\nMenu                           \n1: All tasks                   * - important     \n2: Important only              O - todo     \n3: List manager                X - done\n4: Task manager \nq: quit"
+    putStrLn  "\nWelcome to your HaskMonitor\n\nMenu                           \n1: All tasks                   * - important     \n2: Important only              O - todo     \n3: List manager                X - done\n4: Task manager \nQ: quit"
     action <- getLine
-    if action == "q" then do
+    if map toUpper action == "Q" then do
       putStrLn "Have a nice day!"
       return ()
     else if action == "1" then do
       putStrLn "You chose to go to All tasks."
-      print ((lines contents)!!2)
        
       ---- always available press "..." to go to main menu
       --1 get list of tasks and print them with putStrLn...
@@ -95,13 +129,39 @@ main = do
 
     else if action == "3" then do
       putStrLn "You chose to go to Task manager."
-
+      main' Void -- bara ett exempelvärde inte riktigt klurat ut hur man kan behålla värdet på taskTree genom hela main
     else do
       putStrLn "Sorry that doesn't seen to be an option!"
 
   
 
-    writeFile "Test.txt" (contents ++ "1") -- Uppdaterar textfilen med nya tasks om sådana finns med en hjälpfunktion som lagrar nya tasks i en lista 
+--    writeFile "Test.txt" (contents ++ "1") -- Uppdaterar textfilen med nya tasks om sådana finns med en hjälpfunktion som lagrar nya tasks i en lista 
+
+
+-- 02:57 kom denna som en tankeställare, som ett alternativ till att läsa in från en textfil kan man möjligtvis
+-- behålla ett värde i en 'funktion och uppdatera det regelbundet vid nya tasks. Problemet blir ju att när man 
+-- stänger programmet förloras allt men det kan väl vara ett alternativ, + att den i synnerhet funkar som en utav taskmanager funktionerna
+-- nästa funktion är också synnerligen lätt och det är väldigt lik denna men att man vill ändra om en task är klar och då är det bara att
+-- traversera genom trädet tills input matchar en task och sedan ändra på bool värdet i tuplen. och vidare gäller samma med att radera en task
+-- gonatt gott folk
+
+main' taskTree = do
+        putStrLn "What task would you like to add?"
+        newTask  <- getLine
+        putStrLn "Would you like to add a category, yes or no?" 
+        newCat <- getLine
+        if map toUpper newCat == "YES" then do 
+          putStrLn "What would you like to name the category?"
+          categoryName <- getLine
+          let newTaskTree = insert taskTree categoryName [(newTask, False)]
+          mapM_ print(findAll newTaskTree)
+        else if map toUpper newCat == "NO" then do 
+          let newTaskTree = insert taskTree "" [(newTask, False)]
+          mapM_ print(findAll newTaskTree)
+        else do putStrLn "Sorry that doesn't seen to be an option! Try again!"
+        main
+
+
 
 --getTasks :: Num a => a -> IO()
 --getTasks x = x*x
@@ -123,3 +183,4 @@ main = do
 --------------------------------------------------------------------------------
 testTree = undefined
 runtests = runTestTT $ TestList []
+
